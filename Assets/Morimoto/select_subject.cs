94264708b31752084713a0e7e810346f1b30cb63
morimoto0802 <109467308+morimoto0802@users.noreply.github.com>
2024-03-17 21:34:55 +0900

    2024/3/17
    選んだボタンで配列に授業の駒を入れる。配列に入らない。1番目以降が入らない

--- Assets/Morimoto/select_subject.cs
+++ Assets/Morimoto/select_subject.cs
@@ -12,42 +12,65 @@ public class select_subject : MonoBehaviour
     public Button PE;
     public Button english;
     public Button chemistry;
-
+    string[] plan = new string[4];//配列に今日の授業を入れる
+    int a = 0;
+    void Start()
+    {
+        plan = new string[] {"nothing", "nothing", "nothing", "nothing"}; 
+    }
     public void chose_math()
     {
+        plan[a] = "math";
         Debug.Log("math");
+        a = a + 1;
+        Debug.Log(string.Join(", ", plan));
     }
 
     public void chose_japanese()
     {
+        plan[a] = "japanese";
         Debug.Log("japanese");
+        a = a + 1;
+        Debug.Log(string.Join(", ", plan));
     }
     
     public void chose_PE()
     {
+        plan[a] = "PE";
         Debug.Log("PE");
+        a = a + 1;
+        Debug.Log(string.Join(", ", plan));
     }
     public void chose_english()
     {
+        plan[a] = "english";
         Debug.Log("english");
+        a = a + 1;
+        Debug.Log(string.Join(", ", plan));
     }
     public void chose_physics()
     {
+        plan[a] = "physics";
         Debug.Log("physics");
+        a = a + 1;
+        Debug.Log(string.Join(", ", plan));
     }
     public void chose_history()
     {
+        plan[a] = "history";
         Debug.Log("history");
+        a = a + 1;
+        Debug.Log(string.Join(", ", plan));
     }
     public void chose_chemistry()
     {
+        plan[a] = "chemistry";
         Debug.Log("chemistry");
+        a = a + 1;
+        Debug.Log(string.Join(", ", plan));
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+
 
     // Update is called once per frame
     void Update()
