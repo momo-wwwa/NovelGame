2a4f686b681c7af0dadc5c9c3eb80ee3b6aaf157
morimoto0802 <109467308+morimoto0802@users.noreply.github.com>
2024-03-17 20:44:07 +0900

    2024/3/17

---
+++ Assets/Morimoto/select_subject.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class select_subject : MonoBehaviour
+{
+    public Button math;
+    public Button japanese;
+    public Button physics;
+    public Button history;
+    public Button PE;
+    public Button english;
+    public Button chemistry;
+
+    public void chose_math()
+    {
+        Debug.Log("math");
+    }
+
+    public void chose_japanese()
+    {
+        Debug.Log("japanese");
+    }
+    
+    public void chose_PE()
+    {
+        Debug.Log("PE");
+    }
+    public void chose_english()
+    {
+        Debug.Log("english");
+    }
+    public void chose_physics()
+    {
+        Debug.Log("physics");
+    }
+    public void chose_history()
+    {
+        Debug.Log("history");
+    }
+    public void chose_chemistry()
+    {
+        Debug.Log("chemistry");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
