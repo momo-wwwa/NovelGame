53ac8313face41230b2a6b62570645f729d357dc
ShigetaAyuka <60276109.shigeta.cgp@gmail.com>
2024-03-31 23:20:36 +0900

    補正値に関するプログラムの追加
    シナリオの中で問題を正解したときに補正値をつけて、同授業4回分保持したのちに元に戻すようにした。問題が不正解に補正値がかかっている途中になれば、補正値が戻るようにした。
    ただし、数をミスっていると思われるので要検討

--- Assets/shigeta/script/SubPoint.cs
+++ Assets/shigeta/script/SubPoint.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class SubPoint : MonoBehaviour
 {
-    private float point;
-    private float add;
-    private float crrect;
+    // 別のメソッドでも動くフィールド関数を宣言
+    private double point;
+    private double add;
+    private double crrect;
+    private int counter;
 
     public SubPoint() {
+        // フィールド関数の初期化
         point = 0;
         add = 0;
         crrect = 1;
+        counter = 0;
     }
 
+    // 授業のパラメータ計算
     public void classCalc(int att) {
+
+        // 授業態度による点数の判定
         if (att == 0) {
             add = add + 5;
         }
@@ -25,6 +32,7 @@ public class SubPoint : MonoBehaviour
             add = add + 1;
         }
 
+        // 点数の追加及び上限制御
         if (point < 100) {
             point = point + (add * crrect);
 
@@ -32,6 +40,24 @@ public class SubPoint : MonoBehaviour
                 point = 100;
             }
         }
+
+        // 補正値の管理
+        if (counter >= 1) {
+            crrectCount();
+        }
     }
+
+    // 補正値とカウンターの変更
+    public void crrectCount () {
+        counter += 1;
+        crrect = crrect * 1.2;
+
+        // 初期化
+        if (counter >= 4 || counter <= 0) {
+            counter = 0;
+            crrect = crrect / 1.2;
+        }
+    }
+
 
 }
